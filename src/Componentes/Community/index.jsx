27f1f26045daa0d0d05b4b84ce6a6abaab56b05d
Diego Giraldo <dgiraldogaleano@gmail.com>
2024-05-29 09:01:35 -0500

    Creacion de target membership

--- src/Componentes/Community/index.jsx
+++ src/Componentes/Community/index.jsx
@@ -1,9 +1,19 @@
 import './styles.css'
+import icon_membership from '../../assets/Icon.png'
 function Community() {
     return(
         <>
             <div className='container-title'>
                 <p className='title'>Manage your entire community in a single system</p>
+            </div>
+            <div className=' container-target'>
+                <div>
+                    <img src={icon_membership} alt=""/>
+                    <h1>Membership Organisations</h1>
+                </div>
+                <div>
+                    <p>Our membership management software provides full automation of membership renewals and payments</p>
+                </div>
             </div>
         </>
     )
